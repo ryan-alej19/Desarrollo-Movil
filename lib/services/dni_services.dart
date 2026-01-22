8687b76b256e4261364261fb3fe360dd5eb3f0d9
LENINMORENO13 <ryangallegosmera1@gmail.com>
2026-01-21 19:25:34 -0500

    feat: implement province identification

--- lib/services/dni_services.dart
+++ lib/services/dni_services.dart
@@ -7,4 +7,36 @@ class DniServices {
     final int thirdDigit = int.parse(dni[2]);
     return thirdDigit < 6;
   }
+
+  String getProvince() {
+    final provinceCode = dni.substring(0, 2);
+    final provinces = {
+      '01': 'Azuay',
+      '02': 'Bolívar',
+      '03': 'Cañar',
+      '04': 'Carchi',
+      '05': 'Cotopaxi',
+      '06': 'Chimborazo',
+      '07': 'El Oro',
+      '08': 'Esmeraldas',
+      '09': 'Guayas',
+      '10': 'Imbabura',
+      '11': 'Loja',
+      '12': 'Los Ríos',
+      '13': 'Manabí',
+      '14': 'Morona Santiago',
+      '15': 'Napo',
+      '16': 'Pastaza',
+      '17': 'Pichincha',
+      '18': 'Tungurahua',
+      '19': 'Zamora Chinchipe',
+      '20': 'Galápagos',
+      '21': 'Sucumbíos',
+      '22': 'Orellana',
+      '23': 'Santo Domingo de los Tsáchilas',
+      '24': 'Santa Elena',
+      '30': 'Exterior',
+    };
+    return provinces[provinceCode] ?? 'Desconocida';
+  }
 }
